6dc34adb8b5e5cd88b2bc91ee6a21c52fbc631f9
perkles <andyrodrigo2@gmail.com>
2022-08-24 15:40:14 -0300

    inicial components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './src/app/components/home/home/home.component';
+import { HomeComponent } from './src/app/components/home/home.component';
+import { ItemListComponent } from './src/app/components/item-list/item-list.component';
+import { ItemComponent } from './src/app/components/item/item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ItemListComponent,
+    ItemComponent
   ],
   imports: [
     BrowserModule,
